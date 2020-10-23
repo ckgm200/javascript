4cced29d54e6e2d3c6b72e96b82e42db40cc4db3
min <ckg20020@st.ckg.ac.jp>
2020-10-23 15:45:00 +0900

    202010231530

--- 2020-10-23/2020-1023.js
+++ 2020-10-23/2020-1023.js
@@ -1,12 +1,10 @@
-function translateX(param) {
 
-}
-function translateY(param) {
 
-}
 // Global variable
 var maze;
-
+var MAZESCALE = 51
+var MAZEWALL = 1
+var MAZESTREET = 0
 var mainX, mainY
 
 function random4() {
@@ -17,29 +15,49 @@ function init() {
 
     // alert( "makedata...")
     // 迷路の二次元配列を作る
+    maze = new Array(MAZESCALE)
+    for (var i = 0; j < MAZESCALE; j++) {
+        maze[i] = new Array(MAZESCALE)
 
+    }
     // 全て道にする
-    for (var i = 0; i < 51; i++) {
-        for (var j = 0; j < 51; j++) {
-            maze[i][j] = 0
+    for (var i = 0; i < MAZESCALE; i++) {
+        for (var j = 0; j < MAZESCALE; j++) {
+            maze[i][j] = MAZESTREET
         }
     }
     // 外周部を壁にする
-    for (var i = 0; i < 51; i++) {
-
-
+    for (var i = 0; i < MAZESCALE; i++) {
+        maze[0][i] = MAZEWALL
+        maze[MAZESCALE - 1][i] = MAZEWALL
+        maze[i][0] = MAZEWALL
+        maze[i][MAZESCALE - 1] = MAZEWALL
     }
     //  通れない壁を設定する
-    for (var i = 2; i < 51 - 2; i += 2) {
-        for (var j = 2; j < 51 - 2; j += 2) {
-            maze[i][j] = 1
+    for (var i = 2; i < MAZESCALE - 2; i += 2) {
+        for (var j = 2; j < MAZESCALE - 2; j += 2) {
+            maze[i][j] = MAZEWALL
         }
     }
     // 壁を作る
-    for (var i = 2; i < 51 - 2; i += 2) {
-        for (var j = 2; j < 51 - 2; j += 2) {
+    for (var i = 2; i < MAZESCALE - 2; i += 2) {
+        for (var j = 2; j < MAZESCALE - 2; j += 2) {
             var houkou = random4()
             switch (houkou) {
+                case 1:
+                    maze[i - 1][j] = MAZEWALL
+                    break
+                case 2:
+                    maze[i + 1][j] = MAZEWALL
+                    break
+                case 3:
+                    maze[i][j - 1] = MAZEWALL
+                    break
+                case 4:
+                    maze[i][j + 1] = MAZEWALL
+                    break
+                    
+
 
                 default:
                     alert("error")
@@ -47,8 +65,8 @@ function init() {
         }
     }
     maze[0][0] = maze[1][0] = maze[1][1] = maze[0][1] = 0
-    maze[51 - 1][51 - 1] = maze[51 - 1 - 1][51 - 1] = maze[51 - 1][51 - 1 - 1] = maze[51 - 1 - 1][51 - 1 - 1] = 0
-    
+    maze[MAZESCALE - 1][MAZESCALE - 1] = maze[MAZESCALE - 1 - 1][MAZESCALE - 1] = maze[MAZESCALE - 1][MAZESCALE - 1 - 1] = maze[MAZESCALE - 1 - 1][MAZESCALE - 1 - 1] = 0
+
     // 自分のキャラ位置を設定
     mainX = mainY = 0
     maze[mainX][mainY] = 2
@@ -74,8 +92,8 @@ function drawBlock(x, y) {
     var item = document.getElementById("myCanvas")
     var context = item.getContext("2d")
     if (item.getContext) {
-        for (var i = 0; i < 51; i++) {
-            for (var j = 0; j < 51; j++) {
+        for (var i = 0; i < MAZESCALE; i++) {
+            for (var j = 0; j < MAZESCALE; j++) {
                 switch (maze[x][y]) {
                     case 1:
                         context.fillStyle = "rgb(0, 0, 0)";
@@ -101,8 +119,8 @@ function drawBlock(x, y) {
 }
 function dumpMaze() {
     var str = ""
-    for (var i = 0; i < 51; i++) {
-        for (var j = 0; j < 51; j++) {
+    for (var i = 0; i < MAZESCALE; i++) {
+        for (var j = 0; j < MAZESCALE; j++) {
             str = str + maze[i][j]
         }
         str = str + "\n"
@@ -112,8 +130,8 @@ function dumpMaze() {
 }
 function drawMaze() {
     // alert("button start ...")
-    for (var i = 0; i < 51; i++) {
-        for (var j = 0; j < 51; j++) {
+    for (var i = 0; i < MAZESCALE; i++) {
+        for (var j = 0; j < MAZESCALE; j++) {
             drawBlock(i, j)
         }
     }
