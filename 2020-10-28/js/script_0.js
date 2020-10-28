fed49f14263116da4218ba1281573d527c984b25
min <ckg20020@st.ckg.ac.jp>
2020-10-28 17:08:26 +0900

    2020-1028

--- 2020-10-28/js/script_0.js
+++ 2020-10-28/js/script_0.js
@@ -13,17 +13,22 @@ function drawArray() {
     var context = item.getContext("2d")
     for (var i = 0; i < array.length; i++) {
         if (item.getContext) {
-            
+          context.fillRect(i*10,0,10,10*array[i])  
+          context.fillStyle="rgb(255,255,255)"
+          context.fillRect(i*10,10*(array[i]+1),10,100)
+          context.fillStyle="rgb(0,0,0)"
         }
     }
 }
 
 function bubbleSort(){
-    for ( var  i = 0 ; i < array.length; i++){
+    for ( var  i = 0 ; i < array.length-1; i++){
         for ( var j =i+1 ; j<array.length; j++){
             if ( array[i]>array[j]){
                 // 交換したときに、処理をいったん中断する
-
+                var tnp=array[i]
+                array[i]=array[j]
+                array[j]=tnp
                 return
             }
         }
@@ -32,7 +37,10 @@ function bubbleSort(){
 
 function startSort() {
     // ある時間間隔で実行する処理を定義する
-
+    var rog=function (){
+        drawArray()
+        bubbleSort()
+    }
     // タイマーを使った処理の登録
-
+var timar=setInterval(rog,100)
 }
